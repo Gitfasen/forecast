9f5e770895b01680d8204d76b67297059db5b4aa
alexholub <JustDoIt1000>
2020-11-16 13:42:52 +0200

    change simple functions to arrow functions in api codestyle fixes in components

--- src/api/index.js
+++ src/api/index.js
@@ -6,11 +6,10 @@ import {
   API_SEARCH_CITY_URL,
 } from '../config';
 
-export function searchCity(q) {
-  return new Promise((resolve, reject) => axios.get(`${API_SEARCH_CITY_URL}find?q=${q}&appid=${API_SEARCH_CITY_KEY}&units=metric`)
-    .then((response) => resolve(response.data.list))
-    .catch((err) => reject(err)));
-}
+export const searchCity = async (q) => {
+  const response = await axios.get(`${API_SEARCH_CITY_URL}find?q=${q}&appid=${API_SEARCH_CITY_KEY}&units=metric`);
+  return response.data.list;
+};
 
 const transformData = (data) => ({
   name: data.city.name,
@@ -23,8 +22,7 @@ const transformData = (data) => ({
   }),
 });
 
-export function getForecastByCityId(q) {
-  return new Promise((resolve, reject) => axios.get(`${API_FORECAST_URL}forecast?id=${q}&appid=${API_FORECAST_KEY}&units=metric&cnt=10`)
-    .then((response) => resolve(transformData(response.data)))
-    .catch((err) => reject(err)));
-}
+export const getForecastByCityId = async (q) => {
+  const response = await axios.get(`${API_FORECAST_URL}forecast?id=${q}&appid=${API_FORECAST_KEY}&units=metric&cnt=10`);
+  return transformData(response.data);
+};
